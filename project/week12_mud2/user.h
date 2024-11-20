7e764d14018df9c53e7b0032a1443343448c6b4a
jangyueun <dbdms7773@naver.com>
2024-11-20 22:50:06 +0900

    mudgame

--- project/week12_mud2/user.h
+++ project/week12_mud2/user.h
@@ -11,6 +11,9 @@ public:
     void IncreaseHP(int inc_hp);    // HP 증가하는 멤버함수
     int GetHP();    // HP를 반환하는 멤버함수
     User():hp(20),itemCnt(0){}; // hp=20으로 설정하며 생성하는 생성자
+    void doAttack(){
+        cout<<"공격합니다!"<<endl;
+    }
    
     // << 연산자 중복 정의
     friend ostream& operator<<(ostream& os, const User& user) {
@@ -21,4 +24,20 @@ public:
         itemCnt++;
     }
 
+};
+
+// Magician 클래스
+class Magician : public User {
+public:
+    void doAttack() override { // doAttack 함수 재정의
+        cout << "마법 사용!" << endl;
+    }
+};
+
+// Warrior 클래스
+class Warrior : public User {
+public:
+    void doAttack() override { // doAttack 함수 재정의
+        cout << "베기 사용!" << endl;
+    }
 };
